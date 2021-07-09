81b21d812e328f6a4e55a104c563a08e51f6f123
dtdat2 <dattienpt@gmail.com>
2021-07-09 17:30:05 +0700

    update 9/7

--- src/components/template/InputPushTemplate.vue
+++ src/components/template/InputPushTemplate.vue
@@ -91,25 +91,104 @@ export default {
       emailTitle: "",
       emailContnet: "",
       dataQuery: "",
-      dataParamPush:[]
+      dataParamPush: [],
     };
   },
   methods: {
     clickConvert() {
-      // const regexContent= (\S*[A-Za-z_]\S*[_]\S*)\b;
-      // console.log('scenario:::', this.scenario);
-      // console.log('pushTitle:::', this.pushTitle);
-      // console.log('pushContent:::', this.pushContent);
-
-      console.log("myArray::::", this.getParamPushContent(this.pushContent));
+      console.log("myArray::::", this.getParameter(this.pushContent));
       console.log("myArray::::", this.replacePushContent(this.pushContent));
-      console.log("myArray::::", this.replaceScenarioId(this.scenario));
-      this.dataParamPush = this.getParamPushContent(this.pushContent);
-      this.dataQuery = this.dataParamPush.filter((item, index) => this.dataParamPush.indexOf(item) !== index);
+
+      // console.log("myArray::::", this.replaceScenarioId(this.scenario));
+      // this.dataParamPush = this.getParameter(this.pushContent);
+      // console.log("this.dataParamPush:::", this.dataParamPush);
+
+      let getId = this.replaceScenarioId(this.scenario);
+
+      // GET QUERY SQL EVENTASSOSIATE
+      this.dataParamPush = this.mapEventAssociate(getId);
+
+      // GET QUERY SQL PUSH TEMPLATE
+      // let getContentPush = this.replacePushContent(this.pushContent);
+      // let getParamPush = this.getParameter(this.pushContent);
+      // this.dataParamPush = this.mapPushTemplate(
+      //   getId,
+      //   this.pushTitle,
+      //   getContentPush
+      // );
+
+      // GET QUERY SQL PUSH PADDING
+      // let getParamPush = this.getParameter(this.pushContent);
+      // this.dataParamPush = this.mapPushPadding(getId, getParamPush);
+
+      this.dataQuery = this.dataParamPush;
     },
-    getParamPushContent(param) {
-      let getParam = /S*[A-Za-z_]\S*[_]\S*\b/g;
-      return param.match(getParam);
+
+    mapEventAssociate(param) {
+      let querySQL = [
+        `INSERT INTO event_associate_template(ID, EVENT, EVENT_DESC, NOTICE_TYPE, SEND_BY_TEMPLATE, EVENT_TYPE, IS_ALL_SEND, IS_SEND_BATCH, SEND_OBJECT, RECEIVER_WAY, DEFAULT_SEND_FLAG, TEST_TEMPLATE_ID, PRD_TEMPLATE_ID, MARKET_FLAG, SEND_TIME, IS_DELAY_TYPE, CREATE_DATE, UPDATE_DATE, CREATE_BY, UPDATE_BY) VALUES ('${param}_sms', '${param}', '${param}', '01', 'Y', '01', 'N', 'N', NULL, '05', 'Y', 'sms_${param}', 'sms_${param}', 'N', NULL, 'N', now(), NULL, 'ex-hoangxuanthap001', NULL);`,
+        `INSERT INTO event_associate_template(ID, EVENT, EVENT_DESC, NOTICE_TYPE, SEND_BY_TEMPLATE, EVENT_TYPE, IS_ALL_SEND, IS_SEND_BATCH, SEND_OBJECT, RECEIVER_WAY, DEFAULT_SEND_FLAG, TEST_TEMPLATE_ID, PRD_TEMPLATE_ID, MARKET_FLAG, SEND_TIME, IS_DELAY_TYPE, CREATE_DATE, UPDATE_DATE, CREATE_BY, UPDATE_BY) VALUES ('${param}_email', '${param}', '${param}', '02', 'Y', '01', 'N', 'N', NULL, '05', 'Y', 'email_${param}', 'email_${param}', 'N', NULL, 'N', now(), NULL, 'ex-hoangxuanthap001', NULL);`,
+        `INSERT INTO event_associate_template(ID, EVENT, EVENT_DESC, NOTICE_TYPE, SEND_BY_TEMPLATE, EVENT_TYPE, IS_ALL_SEND, IS_SEND_BATCH, SEND_OBJECT, RECEIVER_WAY, DEFAULT_SEND_FLAG, TEST_TEMPLATE_ID, PRD_TEMPLATE_ID, MARKET_FLAG, SEND_TIME, IS_DELAY_TYPE, CREATE_DATE, UPDATE_DATE, CREATE_BY, UPDATE_BY) VALUES ('${param}_push', '${param}', '${param}', '03', 'Y', '01', 'N', 'N', NULL, '05', 'Y', 'push_${param}', 'push_${param}', 'N', NULL, 'N', now(), NULL, 'ex-hoangxuanthap001', NULL);`,
+      ];
+      return querySQL;
+    },
+    mapPushTemplate(id, pushTitle, pushContent) {
+      let querySQL = [
+        `INSERT INTO push_notification_template(TEMPLATE_ID, SUBJECT, CONTENT, STATE, CREATE_DATE, UPDATE_DATE, CREATE_BY, UPDATE_BY) VALUES ('push_${id}', '${pushTitle}', '${pushContent}', '2', '2021-04-22 17:41:33', '2021-04-22 17:41:33', 'ex-nguyenvinhnam', 'ex-nguyenvinhnam');`,
+      ];
+      return querySQL;
+    },
+    mapSmsTemplate(id, pushTitle, pushContent) {
+      let querySQL = [
+        `INSERT INTO push_notification_template(TEMPLATE_ID, SUBJECT, CONTENT, STATE, CREATE_DATE, UPDATE_DATE, CREATE_BY, UPDATE_BY) VALUES ('sms_${id}', '${pushTitle}', '${pushContent}', '2', '2021-04-22 17:41:33', '2021-04-22 17:41:33', 'ex-nguyenvinhnam', 'ex-nguyenvinhnam');`,
+      ];
+      return querySQL;
+    },
+    mapEmailTemplate(id, pushTitle, pushContent) {
+      let querySQL = [
+        `INSERT INTO email_template(TEMPLATE_ID, SUBJECT, CONTENT, STATE, CREATE_DATE, UPDATE_DATE, CREATE_BY, UPDATE_BY) VALUES ('email_${id}', '${pushTitle}', '${pushContent}', '2', '2021-04-22 17:41:33', '2021-04-22 17:41:33', 'ex-nguyenvinhnam', 'ex-nguyenvinhnam');`,
+      ];
+      return querySQL;
+    },
+    mapPushPadding(id, pushParam) {
+      let queryData = [];
+      for (let i = 0; i < pushParam.length; i++) {
+        const element = pushParam[i];
+        let query = [
+          `INSERT INTO template_data_padding(ID, EVENT, TEST_TEMPLATE_ID, PRD_TEMPLATE_ID, PADDING_ORDER, SOURCE_KEY, TAGET_KEY, CREATE_DATE, UPDATE_DATE, CREATE_BY, UPDATE_BY) VALUES ('push_${id}', '${element}', 'pus_${element}', 'push_${element}', ${i+1}, '${element}', '${element}', NULL, NULL, NULL, NULL);`,
+        ];
+        queryData.push(query);
+      }
+      return queryData;
+    },
+    mapSmsPadding(id, pushParam) {
+      let queryData = [];
+      for (let i = 0; i < pushParam.length; i++) {
+        const element = pushParam[i];
+        let query = [
+          `INSERT INTO template_data_padding(ID, EVENT, TEST_TEMPLATE_ID, PRD_TEMPLATE_ID, PADDING_ORDER, SOURCE_KEY, TAGET_KEY, CREATE_DATE, UPDATE_DATE, CREATE_BY, UPDATE_BY) VALUES ('sms_${id}', '${element}', 'sms_${element}', 'sms_${element}', ${i+1}, '${element}', '${element}', NULL, NULL, NULL, NULL);`,
+        ];
+        queryData.push(query);
+      }
+      return queryData;
+    },
+    mapEmailPadding(id, pushParam) {
+      let queryData = [];
+      for (let i = 0; i < pushParam.length; i++) {
+        const element = pushParam[i];
+        let query = [
+          `INSERT INTO template_data_padding(ID, EVENT, TEST_TEMPLATE_ID, PRD_TEMPLATE_ID, PADDING_ORDER, SOURCE_KEY, TAGET_KEY, CREATE_DATE, UPDATE_DATE, CREATE_BY, UPDATE_BY) VALUES ('email_${id}', '${element}', 'email_${element}', 'email_${element}', ${i}, '${element}', '${element}', NULL, NULL, NULL, NULL);`,
+        ];
+        queryData.push(query);
+      }
+      return queryData;
+    },
+
+    getParameter(param) {
+      let regexParam = /S*[A-Za-z_]\S*[_]\S*\b/g;
+      let getParam = param.match(regexParam);
+      let uniq = [...new Set(getParam)];
+      return uniq;
     },
     replacePushContent(param) {
       let getParam = /S*[A-Za-z_]\S*[_]\S*\b/g;
